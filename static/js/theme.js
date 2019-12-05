44c90ebc3279151c45942638f7dbf69c9ba631eb
Anderson Mendes <anderson2000ano@gmail.com>
2019-12-05 13:24:01 -0300

    initial changes for clarity

--- static/js/theme.js
+++ static/js/theme.js
@@ -33,7 +33,7 @@ const defaultColors = {
 const backgrounds = ['#ffffff', '#212121', '#000000']
 
 const colors = [
-  "#5485d8", "#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5",
+  "#FAB711", "#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5",
   "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
   "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800",
   "#FF5722", "#9E9E9E"
